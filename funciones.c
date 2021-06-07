4fcc53bb8273d61a5ea484b54904b43d16f83254
brenjmv <76862244+brenjmv@users.noreply.github.com>
2021-06-07 13:12:12 -0500

    Update funciones.c

--- funciones.c
+++ funciones.c
@@ -1,30 +1,30 @@
  #include <stdio.h>
  int factorial(int n);
  
- int factorial(int n)
- {
- int i, 
- producto = 1;
- for (i = n; i > 1; --i) {
- producto *= i;
+  int factorial(int n)
+  {
+     int i, 
+     producto = 1;
+     for (i = n; i > 1; --i) {
+     producto *= i;
  }
- return (producto);
+    return (producto);
  }
 
  int main(void)
  {
- int num, fact;
- printf("Dar un entero mayor a 0> ");
- scanf("%d", &num);
- if (num < 0) {
- printf("El factorial de un numero negativo (%d) es indefinido\n", num);
+      int num, fact;
+      printf("Dar un entero mayor a 0 y menor o igual a 99> ");
+     scanf("%d", &num);
+   if (num < 0) {
+     printf("El factorial de un numero negativo (%d) es indefinido\n", num);
  }
- else if (num <= 30) {
- fact = factorial(num);
- printf("El factorial de %d es %d\n", num, fact);
+ else if (num <= 99) {
+    fact = factorial(num);
+     printf("El factorial de %d es %d\n", num, fact);
  }
  else {
- printf("Numero fuera de rango: %d\n", num);
+      printf("Numero fuera de rango: %d\n", num);
  }
- return (0);
+   return (0);
  }
